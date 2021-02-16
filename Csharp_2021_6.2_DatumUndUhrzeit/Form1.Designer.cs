da5e16ecdb2d416c663f603c0e0b10af04f963fa
Benedikt Geisler <benedikt.geisler@posteo.de>
2021-02-16 15:29:27 +0100

    Projektdateien hinzufügen.

---
+++ Csharp_2021_6.2_DatumUndUhrzeit/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Csharp_2021_6._2_DatumUndUhrzeit
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.datDatum = new System.Windows.Forms.DateTimePicker();
+			this.datUhrzeit = new System.Windows.Forms.DateTimePicker();
+			this.SuspendLayout();
+			// 
+			// datDatum
+			// 
+			this.datDatum.Location = new System.Drawing.Point(12, 12);
+			this.datDatum.Name = "datDatum";
+			this.datDatum.Size = new System.Drawing.Size(200, 20);
+			this.datDatum.TabIndex = 0;
+			this.datDatum.ValueChanged += new System.EventHandler(this.datDatum_ValueChanged);
+			// 
+			// datUhrzeit
+			// 
+			this.datUhrzeit.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+			this.datUhrzeit.Location = new System.Drawing.Point(12, 38);
+			this.datUhrzeit.Name = "datUhrzeit";
+			this.datUhrzeit.ShowUpDown = true;
+			this.datUhrzeit.Size = new System.Drawing.Size(200, 20);
+			this.datUhrzeit.TabIndex = 1;
+			this.datUhrzeit.ValueChanged += new System.EventHandler(this.datUhrzeit_ValueChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(373, 202);
+			this.Controls.Add(this.datUhrzeit);
+			this.Controls.Add(this.datDatum);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DateTimePicker datDatum;
+		private System.Windows.Forms.DateTimePicker datUhrzeit;
+	}
+}
+
---
+++ Csharp_2021_6.2_DatumUndUhrzeit/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_2021_6._2_DatumUndUhrzeit
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			Console.WriteLine("DateTime.Now: " + DateTime.Now);
+			Console.WriteLine("DateTime.Today: " + DateTime.Today);
+			Console.WriteLine("Datum kurz: " + DateTime.Today.ToShortDateString());
+			Console.WriteLine("Uhrzeit kurz: " + DateTime.Now.TimeOfDay);
+			Console.WriteLine("Uhrzeit kurz: " + DateTime.Now.ToShortTimeString());
+
+			// Eigenes Datum erstellen
+			DateTime meinDatum = new DateTime(2021, 02, 16);
+			Console.WriteLine("Eigenes Datum anzeigen:");
+			Console.WriteLine(meinDatum);
+			Console.WriteLine(meinDatum.ToShortDateString());
+
+			// Tag der Woche ausgeben
+			Console.WriteLine("Tag der Woche: " + meinDatum.DayOfWeek);
+			Console.WriteLine("Tag des Jahres: " +  meinDatum.DayOfYear);
+
+			// Mit Datum & Uhrzeit rechnen
+			meinDatum = meinDatum.AddHours(3);
+			Console.WriteLine("Nach Addition von 3 Stunden: " + meinDatum);
+
+			meinDatum = meinDatum.AddDays(2);
+			Console.WriteLine("Nach Addition von 2 Tagen: " + meinDatum);
+
+			meinDatum = meinDatum.AddDays(-5);
+			Console.WriteLine("Nach Subtraktion von 5 Tagen: " + meinDatum);
+
+			// Daten miteinander verrechnen
+			DateTime d1 = DateTime.Now;
+			DateTime d2 = new DateTime(2005, 02, 14);
+
+			var ergebnis = d1.Subtract(d2);
+			Console.WriteLine("Die Differenz der Daten beträgt: " + ergebnis);
+			Console.WriteLine("Die Differenz beträgt: " +
+				ergebnis.Days + " Tage, " + 
+				ergebnis.Hours + " Stunden, " + 
+				ergebnis.Minutes + " Minuten");
+
+
+			var differenzInJahren = d1.Year - d2.Year;
+			var restdifferenz = d1.Subtract(new DateTime(d1.Year, 5, 19));
+			Console.WriteLine("Differenz in Jahren: " + differenzInJahren);
+			Console.WriteLine("Restdifferenz: " + restdifferenz);
+		}
+
+		private void datDatum_ValueChanged(object sender, EventArgs e)
+		{
+			Console.WriteLine(datDatum.Value.ToShortDateString());
+		}
+
+		private void datUhrzeit_ValueChanged(object sender, EventArgs e)
+		{
+			Console.WriteLine(datUhrzeit.Value.ToShortTimeString());
+		}
+	}
+}
